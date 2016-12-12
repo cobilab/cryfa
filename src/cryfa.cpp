61824c93aaf75c78354187786ce9203fde7b7e14
pratas <pratas@ua.pt>
2016-12-12 19:42:19 +0000

    flagsMinor

--- src/cryfa.cpp
+++ src/cryfa.cpp
@@ -233,51 +233,6 @@ int main(int argc, char* argv[]){
     std::cerr << "Verbose mode on.\n";
     }
 
-/*
-  //Key and IV setup
-  //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-   
-  //bit). This key is secretly exchanged between two parties before communication   
-  //begins. DEFAULT_KEYLENGTH= 16 bytes
-  byte key[CryptoPP::AES::MAX_KEYLENGTH], 
-        iv[CryptoPP::AES::BLOCKSIZE];
-  memset(key, 0x00, CryptoPP::AES::MAX_KEYLENGTH);
-  memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE);
-
-  //
-  // String and Sink setup
-  //
-  std::string plaintext = "Now is the time for all good men to come to the aide...";
-  std::string ciphertext;
-  std::string decryptedtext;
-
-  //
-  // Dump Plain Text
-  //
-  std::cerr << "Plain Text (" << plaintext.size() << " bytes)" << std::endl;
-  std::cout << plaintext;
-  std::cout << std::endl << std::endl;
-
-  //
-  // Create Cipher Text
-  //
-  CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::MAX_KEYLENGTH);
-  CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
-  CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink( ciphertext ) );
-  stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() + 1 );
-  stfEncryptor.MessageEnd();
-
-  //
-  // Dump Cipher Text
-  //
-  std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
-
-  for(int i = 0; i < ciphertext.size(); ++i){
-    std::cout << "0x" << std::hex << (0xFF & static_cast<byte>(ciphertext[i])) << " ";
-    }
-
-  std::cout << std::endl << std::endl;
-*/
-
   if(d_flag){
     std::cerr << "Decryption mode on.\n";
     DecryptFA(argc, argv);
