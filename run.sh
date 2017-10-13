45c3f7a27c3c55201b462fb3244b740b621c5b89
Morteza <s_morteza_h@yahoo.com>
2017-10-13 16:33:25 +0100

    Minor changes

--- run.sh
+++ run.sh
@@ -1257,78 +1257,195 @@ then
       split(c_arrMinSys[2], c_arrSecSys, "s");      c_secSys=c_arrSecSys[1];
       c_sysTime=c_minSys*60+c_secSys;
       c_cpuTime=c_userTime+c_sysTime;
-      printf "\t%.3f\t%.f", c_cpuTime, $8;
+      printf "\t%.3f", c_cpuTime;
+
+      printf "\t%.f", $8;
 
       split($9, d_arrMinReal, "m");                 d_minReal=d_arrMinReal[1];
       split(d_arrMinReal[2], d_arrSecReal, "s");    d_secReal=d_arrSecReal[1];
       d_realTime=d_minReal*60+d_secReal;
       printf "\t%.3f", d_realTime;
 
-      split($10, d_arrMinUser, "m");                d_minUser=d_arrMinUser[1];
-      split(d_arrMinUser[2], d_arrSecUser, "s");    d_secUser=d_arrSecUser[1];
+      split($10, d_arrMinUser, "m");               d_minUser=d_arrMinUser[1];
+      split(d_arrMinUser[2], d_arrSecUser, "s");  d_secUser=d_arrSecUser[1];
       d_userTime=d_minUser*60+d_secUser;
-      split($11, d_arrMinSys, "m");                 d_minSys=d_arrMinSys[1];
-      split(d_arrMinSys[2], d_arrSecSys, "s");      d_secSys=d_arrSecSys[1];
+      split($11, d_arrMinSys, "m");                d_minSys=d_arrMinSys[1];
+      split(d_arrMinSys[2], d_arrSecSys, "s");    d_secSys=d_arrSecSys[1];
       d_sysTime=d_minSys*60+d_secSys;
       d_cpuTime=d_userTime+d_sysTime;
-      printf "\t%.3f\t%.f\t%d\n", d_cpuTime, $12, $13;
+      printf "\t%.3f", d_cpuTime;
+
+      printf "\t%.f\t%d\n", $12, $13;
       }' >> $INWF.tmp
 
       ### FASTA
-      # 1 row for headers and 1 row after all. Cryfa included
+      # details -- 1 row for headers and 1 row after all
       removeFromRow=`echo $((FASTA_DATASET_SIZE*(FASTA_METHODS_SIZE+1)+1+1))`
       sed "$removeFromRow,$ d" $INWF.tmp > ${INWF}_detail_FA.$INF;
 
-      sed "2,$ d" ${INWF}_detail_FA.$INF | cut -f2- > ${INWF}_ave_FA.$INF;
+      # for each dataset
+      C_each="UnC_Ratio\tC_Speed(MB/s)\tC_Time_cpu(m)\tC_Mem(MB)"
+      D_each="D_Speed(MB/s)\tD_Time_cpu(m)\tD_Mem(MB)"
+      printf "Dataset\tSize(MB)\tC_Method\t$C_each\t$D_each\tEq\n" \
+          > ${INWF}_FA.tmp
+      cat ${INWF}_detail_FA.$INF | awk 'NR>1' | awk 'BEGIN{}{
+      printf "%s\t%.f\t%s\t%.1f\t%.1f\t%.2f\t%.f\t%.1f\t%.2f\t%.f\t%.1f\n",
+       $1, $2/(1024*1024), $3, $2/($2-$4), $2/(1024*1024*$5), $6/60,
+       $7/1024, $4/(1024*1024*$8), $9/60, $10/1024, $11;
+       }'  >> ${INWF}_FA.tmp
+
+       for i in $FASTA_DATASET; do
+           sed "2,$ d" ${INWF}_FA.tmp > ${INWF}_${i}_FA.$INF;
+           cat ${INWF}_FA.tmp | awk 'NR>1' \
+            | awk -v i=$i 'BEGIN{}{if ($1==i) print;}' >> ${INWF}_${i}_FA.$INF;
+       done
+       rm ${INWF}_FA.tmp
+
+      # total
+      C_tot="UnC_Ratio\tC_Speed(MB/s)\tC_Time_cpu(m)"
+      D_tot="D_Speed(MB/s)\tD_Time_cpu(m)"
+      printf "Size(MB)\tC_Method\t$C_tot\t$D_tot\tEq\n" \
+          > ${INWF}_tot_FA.$INF;
       cat ${INWF}_detail_FA.$INF | awk 'NR>1' \
        | awk -v dsSize=$FASTA_DATASET_SIZE 'BEGIN{}{
-       s+=$2;      cS+=$4;     cTR+=$5;    cTC+=$6;    cM+=$7;
-       dTR+=$8;    dTC+=$9;    dM+=$10;    eq+=$11;
+       s+=$2;  cS+=$4;  cTR+=$5;  cTC+=$6;  dTR+=$8;  dTC+=$9;  eq+=$11;
        if (NR % dsSize==0) {
-        printf "%.3f\t%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.2f\n",
-               s/dsSize, $3, cS/dsSize, cTR/dsSize, cTC/dsSize,
-               cM/dsSize, dTR/dsSize, dTC/dsSize, dM/dsSize, eq/dsSize;
-        s=0;   cS=0;   cTR=0;   cTC=0;   cM=0;   dTR=0;   dTC=0;   dM=0;   eq=0;
+        printf "%.f\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",
+               s/(1024*1024), $3, s/(s-cS), s/(1024*1024*cTR),
+               cTC/60, cS/(1024*1024*dTR), dTC/60, eq;
+        s=0;   cS=0;   cTR=0;   cTC=0;   dTR=0;   dTC=0;   eq=0;
        }
-       }' >> ${INWF}_ave_FA.$INF
-
-      c_fa="UnC_Ratio\tC_Speed(MB/s)\tC_Time_cpu(m)\tC_Mem(MB)"
-      d_fa="D_Speed(MB/s)\tD_Time_cpu(m)\tD_Mem(MB)"
-      printf "Size(MB)\tC_Method\t$c_fa\t$d_fa\tEq\n" > ${INWF}_FA.$INF
-      cat ${INWF}_ave_FA.$INF | awk 'NR>1' | awk 'BEGIN{}{
-       printf "%.f\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",
-       $1/(1024*1024), $2, $1/($1-$3), $1/(1024*1024*$4), $5/60, $6/1024,
-       $3/(1024*1024*$7), $8/60, $9/1024, $10;
-       }'  >> ${INWF}_FA.$INF
+       }' >> ${INWF}_tot_FA.$INF
 
       ### FASTQ
-      # 1 row for headers and 1 row after all. Cryfa included
+      # details -- 1 row for headers and 1 row after all
       removeUpToRow=`echo $((removeFromRow-1))`
       sed "2,$removeUpToRow d" $INWF.tmp > ${INWF}_detail_FQ.$INF;
 
-      sed "2,$ d" ${INWF}_detail_FQ.$INF | cut -f2- > ${INWF}_ave_FQ.$INF;
+      # for each dataset
+      printf "Dataset\tSize(MB)\tCEn_Method\t$cen_each\t$ded_each\tEq\n" \
+          > ${INWF}_FQ.tmp
+      cat ${INWF}_detail_FQ.$INF | awk 'NR>1' | awk 'BEGIN{}{
+      printf "%s\t%.f\t%s+%s\t%.1f\t%.1f\t%.2f\t%.f\t%.1f\t%.2f\t%.f\t%.1f\n",
+       $1, $2/(1024*1024), $3, $4, $2/($2-$5), $2/(1024*1024*$6), $7/60,
+       $8/1024, $5/(1024*1024*$9), $10/60, $11/1024, $12;
+       }'  >> ${INWF}_FQ.tmp
+
+       for i in $FASTQ_DATASET; do
+           sed "2,$ d" ${INWF}_FQ.tmp > ${INWF}_${i}_FQ.$INF;
+           cat ${INWF}_FQ.tmp | awk 'NR>1' \
+            | awk -v i=$i 'BEGIN{}{if ($1==i) print;}' >> ${INWF}_${i}_FQ.$INF;
+       done
+       rm ${INWF}_FQ.tmp
+
+      # total
+      printf "Size(MB)\tCEn_Method\t$cen_tot\t$ded_tot\tEq\n" \
+          > ${INWF}_tot_FQ.$INF;
       cat ${INWF}_detail_FQ.$INF | awk 'NR>1' \
        | awk -v dsSize=$FASTQ_DATASET_SIZE 'BEGIN{}{
-       s+=$2;      cS+=$5;      cTR+=$6;    cTC+=$7;    cM+=$8;
-       dTR+=$9;    dTC+=$10;    dM+=$11;    eq+=$12;
+       s+=$2; cenS+=$5; cenTR+=$6; cenTC+=$7; dedTR+=$9; dedTC+=$10; eq+=$12;
        if (NR % dsSize==0) {
-        printf "%.3f\t%s\t%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.2f\n",
-               s/dsSize, $3, $4, cS/dsSize, cTR/dsSize, cTC/dsSize,
-               cM/dsSize, dTR, dTC, dM/dsSize, eq/dsSize;
-        s=0; cS=0; cTR=0; cTC=0; cM=0; dTR=0; dTC=0; dM=0; eq=0;
+        printf "%.f\t%s+%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",
+               s/(1024*1024), $3, $4, s/(s-cenS), s/(1024*1024*cenTR),
+               cenTC/60, cenS/(1024*1024*dedTR), dedTC/60, eq;
+        s=0;  cenS=0;  cenTR=0;  cenTC=0;  dedTR=0;  dedTC=0;  eq=0;
        }
-       }' >> ${INWF}_ave_FQ.$INF
-
-      c_fq="UnC_Ratio\tC_Speed(MB/s)\tC_Time_cpu(m)\tC_Mem(MB)"
-      d_fq="D_Speed(MB/s)\tD_Time_cpu(m)\tD_Mem(MB)"
-      printf "Size(MB)\tC_Method\t$c_fq\t$d_fq\tEq\n" > ${INWF}_FQ.$INF
-      cat ${INWF}_ave_FQ.$INF | awk 'NR>1' | awk 'BEGIN{}{
-       printf "%.f\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",
-       $1/(1024*1024), $2, $1/($1-$3), $1/(1024*1024*$4), $5/60, $6/1024,
-       $3/(1024*1024*$7), $8/60, $9/1024, $10;
-       }'  >> ${INWF}_FQ.$INF
+       }' >> ${INWF}_tot_FQ.$INF
 
       rm $INWF.tmp
+
+
+#      c="C_Size(B)\tC_Time_real(s)\tC_Time_cpu(s)\tC_Mem(KB)"
+#      d="D_Time_real(s)\tD_Time_cpu(s)\tD_Mem(KB)"
+#      printf "Dataset\tSize(B)\tMethod\t$c\t$d\tEq\n" > $INWF.tmp
+#
+#      cat $IN | awk 'NR>1' | tr ',' . | awk 'BEGIN {}{
+#      printf "%s\t%.f\t%s\t%.f", $1, $2, $3, $4;
+#
+#      split($5, c_arrMinReal, "m");                 c_minReal=c_arrMinReal[1];
+#      split(c_arrMinReal[2], c_arrSecReal, "s");    c_secReal=c_arrSecReal[1];
+#      c_realTime=c_minReal*60+c_secReal;
+#      printf "\t%.3f", c_realTime;
+#
+#      split($6, c_arrMinUser, "m");                 c_minUser=c_arrMinUser[1];
+#      split(c_arrMinUser[2], c_arrSecUser, "s");    c_secUser=c_arrSecUser[1];
+#      c_userTime=c_minUser*60+c_secUser;
+#      split($7, c_arrMinSys, "m");                  c_minSys=c_arrMinSys[1];
+#      split(c_arrMinSys[2], c_arrSecSys, "s");      c_secSys=c_arrSecSys[1];
+#      c_sysTime=c_minSys*60+c_secSys;
+#      c_cpuTime=c_userTime+c_sysTime;
+#      printf "\t%.3f\t%.f", c_cpuTime, $8;
+#
+#      split($9, d_arrMinReal, "m");                 d_minReal=d_arrMinReal[1];
+#      split(d_arrMinReal[2], d_arrSecReal, "s");    d_secReal=d_arrSecReal[1];
+#      d_realTime=d_minReal*60+d_secReal;
+#      printf "\t%.3f", d_realTime;
+#
+#      split($10, d_arrMinUser, "m");                d_minUser=d_arrMinUser[1];
+#      split(d_arrMinUser[2], d_arrSecUser, "s");    d_secUser=d_arrSecUser[1];
+#      d_userTime=d_minUser*60+d_secUser;
+#      split($11, d_arrMinSys, "m");                 d_minSys=d_arrMinSys[1];
+#      split(d_arrMinSys[2], d_arrSecSys, "s");      d_secSys=d_arrSecSys[1];
+#      d_sysTime=d_minSys*60+d_secSys;
+#      d_cpuTime=d_userTime+d_sysTime;
+#      printf "\t%.3f\t%.f\t%d\n", d_cpuTime, $12, $13;
+#      }' >> $INWF.tmp
+#
+#      ### FASTA
+#      # 1 row for headers and 1 row after all. Cryfa included
+#      removeFromRow=`echo $((FASTA_DATASET_SIZE*(FASTA_METHODS_SIZE+1)+1+1))`
+#      sed "$removeFromRow,$ d" $INWF.tmp > ${INWF}_detail_FA.$INF;
+#
+#      sed "2,$ d" ${INWF}_detail_FA.$INF | cut -f2- > ${INWF}_ave_FA.$INF;
+#      cat ${INWF}_detail_FA.$INF | awk 'NR>1' \
+#       | awk -v dsSize=$FASTA_DATASET_SIZE 'BEGIN{}{
+#       s+=$2;      cS+=$4;     cTR+=$5;    cTC+=$6;    cM+=$7;
+#       dTR+=$8;    dTC+=$9;    dM+=$10;    eq+=$11;
+#       if (NR % dsSize==0) {
+#        printf "%.3f\t%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.2f\n",
+#               s/dsSize, $3, cS/dsSize, cTR/dsSize, cTC/dsSize,
+#               cM/dsSize, dTR/dsSize, dTC/dsSize, dM/dsSize, eq/dsSize;
+#        s=0;   cS=0;   cTR=0;   cTC=0;   cM=0;   dTR=0;   dTC=0;   dM=0;   eq=0;
+#       }
+#       }' >> ${INWF}_ave_FA.$INF
+#
+#      c_fa="UnC_Ratio\tC_Speed(MB/s)\tC_Time_cpu(m)\tC_Mem(MB)"
+#      d_fa="D_Speed(MB/s)\tD_Time_cpu(m)\tD_Mem(MB)"
+#      printf "Size(MB)\tC_Method\t$c_fa\t$d_fa\tEq\n" > ${INWF}_FA.$INF
+#      cat ${INWF}_ave_FA.$INF | awk 'NR>1' | awk 'BEGIN{}{
+#       printf "%.f\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",
+#       $1/(1024*1024), $2, $1/($1-$3), $1/(1024*1024*$4), $5/60, $6/1024,
+#       $3/(1024*1024*$7), $8/60, $9/1024, $10;
+#       }'  >> ${INWF}_FA.$INF
+#
+#      ### FASTQ
+#      # 1 row for headers and 1 row after all. Cryfa included
+#      removeUpToRow=`echo $((removeFromRow-1))`
+#      sed "2,$removeUpToRow d" $INWF.tmp > ${INWF}_detail_FQ.$INF;
+#
+#      sed "2,$ d" ${INWF}_detail_FQ.$INF | cut -f2- > ${INWF}_ave_FQ.$INF;
+#      cat ${INWF}_detail_FQ.$INF | awk 'NR>1' \
+#       | awk -v dsSize=$FASTQ_DATASET_SIZE 'BEGIN{}{
+#       s+=$2;      cS+=$5;      cTR+=$6;    cTC+=$7;    cM+=$8;
+#       dTR+=$9;    dTC+=$10;    dM+=$11;    eq+=$12;
+#       if (NR % dsSize==0) {
+#        printf "%.3f\t%s\t%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.2f\n",
+#               s/dsSize, $3, $4, cS/dsSize, cTR/dsSize, cTC/dsSize,
+#               cM/dsSize, dTR, dTC, dM/dsSize, eq/dsSize;
+#        s=0; cS=0; cTR=0; cTC=0; cM=0; dTR=0; dTC=0; dM=0; eq=0;
+#       }
+#       }' >> ${INWF}_ave_FQ.$INF
+#
+#      c_fq="UnC_Ratio\tC_Speed(MB/s)\tC_Time_cpu(m)\tC_Mem(MB)"
+#      d_fq="D_Speed(MB/s)\tD_Time_cpu(m)\tD_Mem(MB)"
+#      printf "Size(MB)\tC_Method\t$c_fq\t$d_fq\tEq\n" > ${INWF}_FQ.$INF
+#      cat ${INWF}_ave_FQ.$INF | awk 'NR>1' | awk 'BEGIN{}{
+#       printf "%.f\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",
+#       $1/(1024*1024), $2, $1/($1-$3), $1/(1024*1024*$4), $5/60, $6/1024,
+#       $3/(1024*1024*$7), $8/60, $9/1024, $10;
+#       }'  >> ${INWF}_FQ.$INF
+#
+#      rm $INWF.tmp
   }
 
   # convert memory numbers scale to MB and times to fractional minutes in
