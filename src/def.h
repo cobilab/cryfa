c0a78671d2e4af6c8b13d8db01fe408a016a6362
Morteza <s_morteza_h@yahoo.com>
2017-11-14 15:20:13 +0000

    Minor changes

--- src/def.h
+++ src/def.h
@@ -84,7 +84,7 @@ const   string PK_FILENAME   = "CRYFA_PK";   /**< @brief Packed file name */
 const   string PCKD_FILENAME = "CRYFA_PCKD"; /**< @brief Pckd f name - joined */
 const   string DEC_FILENAME  = "CRYFA_DEC";  /**< @brief Decrypted file name */
 const   string UPK_FILENAME  = "CRYFA_UPK";  /**< @brief Unpacked file name */
-constexpr byte DEFAULT_N_THR = 1;   /**< @brief Default number of threads */
+constexpr byte DEFAULT_N_THR = 8;   /**< @brief Default number of threads */
 constexpr u64  BLOCK_SIZE    = 8 * 1024;  /**< @brief To read from input file */
 constexpr byte C1            = 2;   /**< @brief       Cat 1  =  2 */
 constexpr byte C2            = 3;   /**< @brief       Cat 2  =  3 */
