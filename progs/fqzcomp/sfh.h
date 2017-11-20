2019562c0e58488177e6b9f2c7d24dfddf5c79aa
Morteza <s_morteza_h@yahoo.com>
2017-11-20 15:34:34 +0000

    Minor changes

--- progs/fqzcomp/sfh.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SFH_H_
-#define _SFH_H_
-
-#include <stdint.h>
-
-/* An reimplementation of Super Fast Hash by Paul Hsieh */
-uint32_t sfhash(const unsigned char *d, int len);
-
-#endif /* _SFH_H_ */
--- progs/quip/src/version.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef QUIP_VERSION_H
-#define QUIP_VERSION_H
-
-#define QUIP_VERSION "1.1.8"
-
-#endif
-
