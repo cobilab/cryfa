be329195b66bf6204c70408e857182ae183207a3
Morteza <s_morteza_h@yahoo.com>
2018-01-05 18:25:45 +0000

    Download Plants

--- script/run_res_redun.sh
+++ script/run_res_redun.sh
@@ -8,17 +8,17 @@
 #!/bin/bash
 
 CRYFA=1
-MFCOMPRESS=0
-DELIMINATE=0
+MFCOMPRESS=1
+DELIMINATE=1
+GECO=0
 
 
-#DATA_SET="A B F V P";
-DATA_SET="A";
-printf "Method;Category;Dataset;Size;CompSize;CRatio;CRatioRev\n" > red
+DATA_SET="A B F V P";
 
 ### cryfa
 if [[ $CRYFA -eq 1 ]];
 then
+    printf "Method\tCategory\tDataset\tSize\tCompSize\tCRatio\tCRatioRev\n" > REDUN-CRYFA.dat
     cd progs/cryfa
     echo "              %%%%%%%%%%%%%"
     echo "%%%%%%%%%%%%%%%   CRYFA   %%%%%%%%%%%%%%%"
@@ -30,9 +30,9 @@ then
             inwf="${in%.*}"    # Input file name without filetype
             origSize=`stat --printf="%s" $i`
             compSize=`stat --printf="%s" $i.cryfa`
-            printf "cryfa;$d;$inwf;$origSize;$compSize;%s;%s\n" \
+            printf "cryfa\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
                    "`echo "scale=5; $origSize/$compSize" | bc -l`" \
-                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../red
+                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../REDUN-CRYFA.dat
             rm -f $i.cryfa
         done
     done
@@ -43,6 +43,7 @@ fi
 ### MFCompress
 if [[ $MFCOMPRESS -eq 1 ]];
 then
+    printf "Method\tCategory\tDataset\tSize\tCompSize\tCRatio\tCRatioRev\n" > REDUN-MFCOMPRESS.dat
     cd progs/mfcompress
     echo "            %%%%%%%%%%%%%%%%%%"
     echo "%%%%%%%%%%%%%   MFCOMPRESS   %%%%%%%%%%%%%%%"
@@ -54,9 +55,9 @@ then
             inwf="${in%.*}"    # Input file name without filetype
             origSize=`stat --printf="%s" $i`
             compSize=`stat --printf="%s" $i.mfc`
-            printf "MFCompress;$d;$inwf;$origSize;$compSize;%s;%s\n" \
+            printf "MFCompress\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
                    "`echo "scale=5; $origSize/$compSize" | bc -l`" \
-                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../red
+                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../REDUN-MFCOMPRESS.dat
             rm -f $i.mfc
         done
     done
@@ -67,6 +68,7 @@ fi
 ### DELIMINATE
 if [[ $DELIMINATE -eq 1 ]];
 then
+    printf "Method\tCategory\tDataset\tSize\tCompSize\tCRatio\tCRatioRev\n" > REDUN-DELIMINATE.dat
     cd progs/delim
     echo "            %%%%%%%%%%%%%%%%%%"
     echo "%%%%%%%%%%%%%   DELIMINATE   %%%%%%%%%%%%%%%"
@@ -78,11 +80,35 @@ then
             inwf="${in%.*}"    # Input file name without filetype
             origSize=`stat --printf="%s" $i`
             compSize=`stat --printf="%s" $i.dlim`
-            printf "DELIMINATE;$d;$inwf;$origSize;$compSize;%s;%s\n" \
+            printf "DELIMINATE\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
                    "`echo "scale=5; $origSize/$compSize" | bc -l`" \
-                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../red
+                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../REDUN-DELIMINATE.dat
             rm -f $i.dlim
         done
     done
     cd ../..
 fi
+
+
+
+
+
+#if [[ GECO -eq 1 ]];
+#then
+#    echo "            %%%%%%%%%%%%%%%%%%"
+#    echo "%%%%%%%%%%%%%   GECO   %%%%%%%%%%%%%%%"
+#    echo "            %%%%%%%%%%%%%%%%%%"
+#    for d in $DATA_SET; do
+#        for i in dataset/Redundancy/$d/*.fa; do
+#            ./GeCo -tm 6:1:0:0/0 -tm 13:10:1:0/0 -g 0.8 $i;
+#            in="${i##*/}"      # Input file name
+#            inwf="${in%.*}"    # Input file name without filetype
+#            origSize=`stat --printf="%s" $i`
+#            compSize=`stat --printf="%s" $i.co`
+#            printf "GECO\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
+#                   "`echo "scale=5; $origSize/$compSize" | bc -l`" \
+#                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> red
+#            rm -f $i.co
+#        done
+#    done
+#fi
