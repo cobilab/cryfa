5c6d2d579719fdad9417f518aa750ff1fac0b7b0
Morteza <s_morteza_h@yahoo.com>
2018-01-06 12:08:27 +0000

    Minor changes

--- script/run_res_redun.sh
+++ script/run_res_redun.sh
@@ -10,29 +10,30 @@
 CRYFA=1
 MFCOMPRESS=1
 DELIMINATE=1
-GECO=0
-
 
 DATA_SET="A B F V P";
 
-### cryfa
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#   cryfa
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if [[ $CRYFA -eq 1 ]];
 then
-    printf "Method\tCategory\tDataset\tSize\tCompSize\tCRatio\tCRatioRev\n" > REDUN-CRYFA.dat
+    printf "Method\tCategory\tDataset\tSize\tCompSize\tNRC\n" \
+        > $result/REDUN-CRYFA.$INF
     cd progs/cryfa
     echo "              %%%%%%%%%%%%%"
     echo "%%%%%%%%%%%%%%%   CRYFA   %%%%%%%%%%%%%%%"
     echo "              %%%%%%%%%%%%%"
     for d in $DATA_SET; do
-        for i in ../../dataset/Redundancy/$d/*.fa; do
-            ./cryfa -k pass.txt -t 8 $i > $i.cryfa
+        for i in ../../$dataset/$redun/$d/*.$fasta; do
+            ./cryfa -k $CRYFA_KEY_FILE -t $CRYFA_DEFAULT_N_THR $i > $i.cryfa
             in="${i##*/}"      # Input file name
             inwf="${in%.*}"    # Input file name without filetype
             origSize=`stat --printf="%s" $i`
             compSize=`stat --printf="%s" $i.cryfa`
-            printf "cryfa\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
-                   "`echo "scale=5; $origSize/$compSize" | bc -l`" \
-                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../REDUN-CRYFA.dat
+            printf "cryfa\t$d\t$inwf\t$origSize\t$compSize\t%s\n"      \
+                   "`echo "scale=5; $compSize/($origSize*2)" | bc -l`" \
+                   >> ../../$result/REDUN-CRYFA.$INF
             rm -f $i.cryfa
         done
     done
@@ -40,24 +41,27 @@ then
 fi
 
 
-### MFCompress
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#   MFCompress
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if [[ $MFCOMPRESS -eq 1 ]];
 then
-    printf "Method\tCategory\tDataset\tSize\tCompSize\tCRatio\tCRatioRev\n" > REDUN-MFCOMPRESS.dat
+    printf "Method\tCategory\tDataset\tSize\tCompSize\tNRC\n" \
+        > $result/REDUN-MFCOMPRESS.$INF
     cd progs/mfcompress
     echo "            %%%%%%%%%%%%%%%%%%"
     echo "%%%%%%%%%%%%%   MFCOMPRESS   %%%%%%%%%%%%%%%"
     echo "            %%%%%%%%%%%%%%%%%%"
     for d in $DATA_SET; do
-        for i in ../../dataset/Redundancy/$d/*.fa; do
+        for i in ../../$dataset/$redun/$d/*.$fasta; do
             ./MFCompressC -o $i.mfc $i;
             in="${i##*/}"      # Input file name
             inwf="${in%.*}"    # Input file name without filetype
             origSize=`stat --printf="%s" $i`
             compSize=`stat --printf="%s" $i.mfc`
-            printf "MFCompress\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
-                   "`echo "scale=5; $origSize/$compSize" | bc -l`" \
-                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../REDUN-MFCOMPRESS.dat
+            printf "MFCompress\t$d\t$inwf\t$origSize\t$compSize\t%s\n" \
+                   "`echo "scale=5; $compSize/($origSize*2)" | bc -l`" \
+                   >> ../../$result/REDUN-MFCOMPRESS.$INF
             rm -f $i.mfc
         done
     done
@@ -65,24 +69,27 @@ then
 fi
 
 
-### DELIMINATE
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#   DELIMINATE
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if [[ $DELIMINATE -eq 1 ]];
 then
-    printf "Method\tCategory\tDataset\tSize\tCompSize\tCRatio\tCRatioRev\n" > REDUN-DELIMINATE.dat
+    printf "Method\tCategory\tDataset\tSize\tCompSize\tNRC\n" \
+        > $result/REDUN-DELIMINATE.$INF
     cd progs/delim
     echo "            %%%%%%%%%%%%%%%%%%"
     echo "%%%%%%%%%%%%%   DELIMINATE   %%%%%%%%%%%%%%%"
     echo "            %%%%%%%%%%%%%%%%%%"
     for d in $DATA_SET; do
-        for i in ../../dataset/Redundancy/$d/*.fa; do
+        for i in ../../$dataset/$redun/$d/*.$fasta; do
             ./delim a $i;
             in="${i##*/}"      # Input file name
             inwf="${in%.*}"    # Input file name without filetype
             origSize=`stat --printf="%s" $i`
             compSize=`stat --printf="%s" $i.dlim`
-            printf "DELIMINATE\t$d\t$inwf\t$origSize\t$compSize\t%s\t%s\n" \
-                   "`echo "scale=5; $origSize/$compSize" | bc -l`" \
-                   "`echo "scale=5; $compSize/$origSize" | bc -l`" >> ../../REDUN-DELIMINATE.dat
+            printf "DELIMINATE\t$d\t$inwf\t$origSize\t$compSize\t%s\n" \
+                   "`echo "scale=5; $compSize/($origSize*2)" | bc -l`" \
+                   >> ../../$result/REDUN-DELIMINATE.$INF
             rm -f $i.dlim
         done
     done
