9d0102febc22e528f8098d9dc3cdc12cdd77792a
Morteza Hosseini <s_morteza_h@yahoo.com>
2022-07-18 18:40:51 +0100

    Restructure

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
-rm CMakeCache.txt
-cmake ..
-make -j4
-mv cryfa ..
-mv keygen ..
-cd ..
+# Parameters
+BUILD_TYPE=Release
+BUILD=build
+SOURCE=src
+PARALLEL=8
+
+# Configure CMake
+cmake -B $BUILD -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+
+# Build
+cmake --build $BUILD --parallel $PARALLEL --config $BUILD_TYPE
+
+# Move executables to the main directory
+mv $BUILD/cryfa .
+mv $BUILD/keygen .
